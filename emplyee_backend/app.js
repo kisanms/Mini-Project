b3b5e075cd3f6462a525e565e7e6a7645e878948
Kisan Majumdar <kisanmajumdar44@gmail.com>
2024-10-29 11:15:36 +0530

    user data

--- emplyee_backend/app.js
+++ emplyee_backend/app.js
@@ -70,6 +70,19 @@ app.post("/login-user", async (req, res) => {
     }
   }
 });
+app.post("/userdata", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    const useremail = user.email;
+
+    User.findOne({ email: useremail }).then((data) => {
+      return res.send({ status: "Ok", data: data });
+    });
+  } catch (error) {
+    return res.send({ error: error });
+  }
+});
 
 app.listen(5001, () => {
   console.log("NodeJS server started...");
